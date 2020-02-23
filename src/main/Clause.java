042d7adabe6aec12d6ca627748c8712319ee9bf1
Melissa Welsh <mwelsh2@u.rochester.edu>
2020-02-23 16:43:52 -0500

    Finish Part 1

--- src/main/Clause.java
+++ src/main/Clause.java
@@ -2,6 +2,7 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.LinkedHashSet;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -10,6 +11,13 @@ public class Clause {
     Set<Integer> literals;
 
     public Clause(int ...input) {
+        this.literals = new TreeSet<Integer>();
+        for (int i : input) {
+            literals.add(i);
+        }
+    }
+
+    public Clause(Set<Integer> input) {
         this.literals = new TreeSet<Integer>();
         for (int i : input) {
             literals.add(i);
@@ -28,31 +36,47 @@ public class Clause {
         }
     }
 
-    public static void scanCNF(String cnf_string) {
+    public static Set<Clause> scanCNF(String cnf_string) {
         try {
             File cnf = new File(cnf_string);
             Scanner scan = new Scanner(cnf);
             String line = scan.nextLine();
 
-            while (scan.hasNextLine()) {
-                while (line.startsWith("c")) {
-                    line = scan.nextLine();
-                    
-                }
+            while (!line.startsWith("p")) {
                 line = scan.nextLine();
-                System.out.println(line);
             }
+        
+            int i = scan.nextInt();
+            Set<Clause> sentence = new LinkedHashSet<Clause>();
+            Set<Integer> input = new LinkedHashSet<Integer>();
+            input.add(i);
 
+            while (scan.hasNextInt()) {
+        
+                i = scan.nextInt();
+                while(i != 0) {
+                    input.add(i);
+                    i = scan.nextInt();
+                }
+
+                Clause c = new Clause(input);
+                input.clear();
+                sentence.add(c);
+                
+            }
             scan.close();
+            System.out.println(Part2.stringOfClauses(sentence));
+            return sentence;
+
         } catch (FileNotFoundException e) {
             System.out.println("No file found.");
             e.printStackTrace();
         }
-
+        return null;
     }
 
     public static void main(String args[]) {
-        scanCNF("hole6.cnf.txt");
+        scanCNF("quinn.cnf.txt");
     }
 
 }
